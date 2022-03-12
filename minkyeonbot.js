653ad19dc5e9d0556deafae011a5af877c2f9d74
지민경 <jmkjmk6666@gmail.com>
2022-03-12 22:38:29 +0900

    Update minkyeonbot.js

--- minkyeonbot.js
+++ minkyeonbot.js
@@ -34,8 +34,14 @@ function response(room, msg, sender, isGroupChat, replier, imageDB, packageName)
             }
             replier.reply("1교시 : "+timetable[0]+"\n2교시 : "+timetable[1]+"\n3교시 : "+timetable[2]+"\n4교시 : "+timetable[3]+"\n5교시 : "+timetable[4]+"\n6교시 : "+timetable[5]+"\n7교시 : "+timetable[6]);
         }
+        else if(msg == ".t 토"||msg == ".t 일"){
+            replier.reply("그 주말인거 알면서 꼭 이걸 해봐 어?");
+        }
         else if(msg == ".t 선생님"){
-            replier.reply("박지혜선생님 - 2호관 1층 특성화부\n정영주선생님 - 3호관 3층 교육정보부\n김수인선생님 - 1호관 1층 교육연구부\n박정아선생님 - 2호관 1층 취업부\n이왕렬선생님 - 3호관 4층 소프트웨어과");
+            replier.reply("김수인선생님 - 1호관 1층 교육연구부\n박지혜선생님 - 2호관 1층 특성화부\n박정아선생님 - 2호관 1층 취업부\n전형주선생님 - 2호관 1층 3학년부\n정영주선생님 - 3호관 3층 교육정보부\n이왕렬선생님 - 3호관 4층 소프트웨어과");
+        }
+        else if(msg == ".t 전공실"){
+            replier.reply("공업일반 - 422실(교실)\n게임프로그래밍 - 343실\n데이터베이스프로그래밍 - 422실(교실)\n인공지능과미래사회 - 421실\n진로 - 422실\n스마트문화앱콘텐츠제작 - 421실\n응용프로그래밍개발A - 423실\n응용프로그래밍개발B - 422실");
         }
         else if(msg == ".t 30"){
             replier.reply("30분+5분 시간표\n조회 08:30 ~ 08:40\n1교시 08:40 ~ 09:10\n2교시 09:15 ~ 09:45\n3교시 09:50 ~ 10:20\n4교시 10:25 ~ 10:55\n5교시 11:00 ~ 11:30\n6교시 11:35 ~ 12:05\n7교시 12:10 ~ 12:40\n종례 12:40 ~");
@@ -47,23 +53,28 @@ function response(room, msg, sender, isGroupChat, replier, imageDB, packageName)
             replier.reply("45분+5분 시간표\n조회 08:30 ~ 08:40\n1교시 08:40 ~ 09:25\n2교시 09:30 ~ 10:15\n3교시 10:20 ~ 11:05\n4교시 11:10 ~ 11:55\n점심 12:00 ~ 12:45\n5교시 12:50 ~ 13:35\n6교시 13:40 ~ 14:25\n7교시 14:30 ~ 15:15\n종례 15:15 ~");
         }
         else if(msg == ".t 50"){
-            replier.reply("50분+10분 시간표\n조회 08:30 ~ 08:40\n1교시 08:40 ~ 09:30\n2교시 09:35 ~ 10:25\n3교시 10:30 ~ 11:20\n4교시 11:25 ~ 12:15\n점심 12:15 ~ 13:05\n5교시 13:10 ~ 14:00\n6교시 14:05 ~ 14:55\n7교시 15:00 ~ 15:50\n종례 15:50 ~");
+            replier.reply("50분+5분 시간표\n조회 08:30 ~ 08:40\n1교시 08:40 ~ 09:30\n2교시 09:35 ~ 10:25\n3교시 10:30 ~ 11:20\n4교시 11:25 ~ 12:15\n점심 12:15 ~ 13:05\n5교시 13:10 ~ 14:00\n6교시 14:05 ~ 14:55\n7교시 15:00 ~ 15:50\n종례 15:50 ~");
         }
         else if(msg.startsWith(".t test")){
             let user = msg.split(" ");
-            timeset(user[2], user[3]);
-            let resaulttable = ""
-            for(let i=0;i<15;i++){
-                resaulttable += belltable[i];
-                if(i%2==0 && i!=14 && i!=15)
-                    resaulttable += "\n"+(parseInt(i/2)+2)+"교시 : ";
-                else if(i==14)
-                    resaulttable += "\n";
-                else if(i%2==1  && i!=14 && i!=15){
-                    resaulttable +=" ~ "
+            if(isNaN(user[2]) || isNaN(user[3])){
+            }else{
+                if(((user[2] >= 20 && user[2] <= 60) && (user[3] >= 5 && user[3] <= 10)) && (user[2]%1==0 && user[3]%1==0)){
+                    timeset(user[2], user[3]);
+                    let resaulttable = ""
+                    for(let i=0;i<15;i++){
+                        resaulttable += belltable[i];
+                        if(i%2==0 && i!=14 && i!=15)
+                            resaulttable += "\n"+(parseInt(i/2)+2)+"교시 : ";
+                        else if(i==14)
+                            resaulttable += "\n";
+                        else if(i%2==1  && i!=14 && i!=15){
+                            resaulttable +=" ~ "
+                        }
+                    }
+                    replier.reply("조회 08:30 ~ 08:40\n1교시 8:40 ~ "+resaulttable+"종례 : "+belltable[14]+" ~");
                 }
             }
-            replier.reply("조회 08:30 ~ 08:40\n1교시 8:40 ~ "+resaulttable+"종례 : "+belltable[14]+" ~");
         }
 
         // else if(msg == ".t 1" ||msg == ".t 2" ||msg == ".t 3" ||msg == ".t 4" ||msg == ".t 5" ||msg == ".t 6" ||msg == ".t 7"|| msg == ".t 점심"){
